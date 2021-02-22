bd3e51cdf2e76c30acef503fcd503758a9ac08c9
Helder Oliveira <heldrida@gmail.com>
2021-02-22 21:36:57 +0000

    refactor: 💡 main memory allocation (Lexer)

--- main.zig
+++ main.zig
@@ -1,5 +1,14 @@
+const std = @import("std");
 const repl = @import("repl.zig").repl;
+const Lexer = @import("lexer.zig").Lexer;
 
 pub fn main() !void {
-  try repl();
+  var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+  defer arena.deinit();
+
+  const allocator = &arena.allocator;
+
+  var l = try Lexer.create(allocator, "");
+
+  try repl(l);
 }
